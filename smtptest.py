1194641ce680cda55ad3f67934dcd96db81996a6
Nikolaj Nikolajsen <nikolaj@nikolajsen.net>
2018-10-23 09:50:39 +0200

    Add max-mails feature for sending mails in a loop up

--- smtptest.py
+++ smtptest.py
@@ -7,8 +7,11 @@ Usage: python smtptest.py [options] fromaddress toaddress serveraddress
 Examples:
 	python smtptest.py bob@example.com mary@example.com mail.example.com
 	python smtptest.py --debuglevel 1 --usetls -u bob -p xyzzy "Bob <bob@example.com>" mary@example.com mail.example.com
+	python smtptest.py --debuglevel 1 --usetls -u bob -p xyzzy -m 100 "Bob <bob@example.com>" test000@mailinator.com mail.example.com
 
 At verbose == False and debuglevel == 0, smtptest will either succeed silently or print an error. Setting verbose or a debuglevel to 1 will generate intermediate output.
+
+Using the max-mails feature will make the script sending mails in a loop up until hitting the max-mails number, replacing the digit in front of the @-sign with the iterator from every loop (test1@mailinator.com, test2@mailinator.com, test3@mailinator.com... test#maxmails#@mailinator.com)
 
 See also http://docs.python.org/library/smtplib.html
 
@@ -37,6 +40,8 @@ parser.set_defaults(SMTP_USER="")
 parser.set_defaults(SMTP_PASS="")
 parser.set_defaults(debuglevel=0)
 parser.set_defaults(verbose=False)
+parser.set_defaults(maxmails=0)
+parser.set_defaults(returnpath="")
 
 parser.add_option("-t", "--usetls", action="store_true", dest="usetls", default=False, help="Connect using TLS, default is false")
 parser.add_option("-s", "--usessl", action="store_true", dest="usessl", default=False, help="Connect using SSL, default is false")
@@ -45,6 +50,8 @@ parser.add_option("-u", "--username", action="store", type="string", dest="SMTP_
 parser.add_option("-p", "--password", action="store", type="string", dest="SMTP_PASS", help="SMTP server auth password", metavar="password")
 parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Verbose message printing")
 parser.add_option("-d", "--debuglevel", type="int", dest="debuglevel", help="Set to 1 to print smtplib.send messages", metavar="n")
+parser.add_option("-m", "--max-mails", type="int", dest="maxmails", help="The number of mails we should send", metavar="nnn")
+parser.add_option("-r", "--return-path", action="store", type="string", dest="returnpath", help="You can override the envelope sender address", metavar="someone@example.com")
 
 (options, args) = parser.parse_args()
 if len(args) != 3:
@@ -58,7 +65,7 @@ serveraddr = args[2]
 	
 now = strftime("%Y-%m-%d %H:%M:%S")
 
-msg = "From: %s\r\nTo: %s\r\nSubject: Test Message from smtptest at %s\r\n\r\nTest message from the smtptest tool sent at %s" % (fromaddr, toaddr, now, now)
+msg = "From: %s\r\nTo: ###TOADDR###\r\nSubject: Test Message from smtptest at %s\r\n\r\nTest message from the smtptest tool sent at %s" % (fromaddr, now, now)
 
 if options.verbose:
 	print('usetls:', options.usetls)
@@ -70,9 +77,8 @@ if options.verbose:
 	print('smtp username:', options.SMTP_USER)
 	print('smtp password: *****')
 	print('smtplib debuglevel:', options.debuglevel)
-	print("-- Message body ---------------------")
-	print(msg)
-	print("-------------------------------------")
+	print('max mails iterator:', options.maxmails)
+	print('return path override:', options.returnpath)
 
 server = None
 if options.usessl:
@@ -86,5 +92,22 @@ server.ehlo()
 if options.usetls: server.starttls()
 server.ehlo()
 if options.SMTP_USER != "": server.login(options.SMTP_USER, options.SMTP_PASS)
-server.sendmail(fromaddr, toaddr, msg)
+if options.returnpath != "":
+	thisorig = options.returnpath
+else:
+	thisorig = fromaddr
+if options.maxmails > 0:
+	for mailcount in range (1, options.maxmails + 1):
+		thisrcpt = toaddr.replace('000', str(mailcount))
+		thismsg = msg.replace('###TOADDR###', thisrcpt, 1)
+		print("-- Message body ---------------------")
+		print(thismsg)
+		print("-------------------------------------")
+		server.sendmail(thisorig, thisrcpt, thismsg)
+else:
+	thismsg = msg.replace('###TOADDR###', toaddr, 1)
+	print("-- Message body ---------------------")
+	print(thismsg)
+	print("-------------------------------------")
+	server.sendmail(thisorig, toaddr, thismsg)
 server.quit()
